bfaaeed74c73385993097014680895c8e2b706a3
Logen Mo <logen.mo@lixinchuxing.com>
2018-10-22 01:57:01 +0800

    fix: 照片上传优化完成

--- src/pages/manage/goodsUpdata.vue
+++ src/pages/manage/goodsUpdata.vue
@@ -40,10 +40,10 @@
     <wux-popup position="bottom" title="添加种类" class-names="slideInUp" :visible="styleAddShow" @close.stop="onClose">
       <div class="my_popup">
         <div class="my_popup_title">种类标题：</div>
-        <input :input="addItem.name" @input="addItemName" title="种类标题：" placeholder="请输入" />
+        <input @input="addItemName" title="种类标题：" placeholder="请输入" />
       </div>
       <div class="add_item_info">
-        <image class="img" :src="addItem.src" @click="uploadImg('style')"></image>
+        <image class="img" :src="addItem.src" @click="uploadStyle"></image>
         <button @click="finish" type="primary" class="fixed_btnggg">确定</button>
       </div>
     </wux-popup>
@@ -119,12 +119,12 @@ export default {
     },
     finish() {
       if (this.addItem.name && this.addItem.src) {
-        let item = {...this.addItem}
+        let item = { ...this.addItem }
         this.formData.style.push(item)
       }
       this.onClose()
     },
-    uploadImg(key, i) { // 上传图片
+    uploadStyle() {
       if (!this.formData.id) {
         wx.showToast({
           icon: 'none',
@@ -132,55 +132,96 @@ export default {
         })
         return
       }
-      if (key === 'style' && !this.addItem.name) {
+      if (!this.addItem.name) {
         wx.showToast({
           icon: 'none',
-          title: '请输入产品风格',
+          title: '请输入种类标题',
         })
         return
       }
-      let _id = this.formData.id
-      let _num = this.formData[key].length + 1
-      let _this = this
+      this.upload(1, (arr) => {
+        if (!arr || arr.length === 0) return
+        this.addItem.src = arr[0]
+      })
+    },
+    uploadImg(key, i) {
+      let l = 1
+      if (!this.formData.id) {
+        wx.showToast({
+          icon: 'none',
+          title: '请输入产品ID',
+        })
+        return
+      }
+      if (!i) { // 单选
+        if (key === 'swiper') l = 4 - this.swiperLen
+        if (key === 'others') l = 9
+      }
+      this.upload(l, (arr) => {
+        if (!arr || arr.length === 0) return
+        if (i) {
+          this.formData[key][i].src = arr[0]
+        } else {
+          let _id = this.formData.id
+          let _num = this.formData[key].length
+          arr.forEach((v, i) => {
+            let len = _num + i + 1
+            this.formData[key].push({
+              name: `${_id}_${key}_${len}`,
+              src: v
+            })
+          })
+        }
+      })
+    },
+    upload(num = 1, callback) { // 上传图片
+      if (num >= 9) num = 9
       wx.chooseImage({
-        count: 1,
+        count: num,
         sizeType: ['compressed'],
         sourceType: ['album', 'camera'],
         success: (res) => {
           wx.showLoading({
-            title: '上传中',
+            title: '上传中'
           })
-          const filePath = res.tempFilePaths[0]
-          const timestamp = new Date().getTime()
-          // 上传图片
-          const cloudPath = 'images/' + timestamp + filePath.match(/\.[^.]+?$/)[0]
-          wx.cloud.uploadFile({
-              cloudPath,
-              filePath
-            })
-            .then(res => {
-              wx.hideLoading()
-              const src = res.fileID
-              if (i) {
-                _this.formData[key][i].src = src
-                return
-              }
-              if (key === 'style') { // 种类照片上传
-                _this.addItem.src = src
-              } else {
-                _this.formData[key].push({
-                  name: `${key}_${_id}_${_num}`,
-                  src: src
-                })
-              }
-            })
-            .catch(error => {
-              wx.hideLoading()
-              wx.showToast({
-                icon: 'none',
-                title: '上传失败',
+          let i = 0
+          let list = []
+          let timer = null
+          let len = res.tempFilePaths.length
+          let upload = function(l) {
+            const filePath = res.tempFilePaths[i]
+            const timestamp = new Date().getTime()
+            // 上传图片
+            const cloudPath = 'images/' + timestamp + filePath.match(/\.[^.]+?$/)[0]
+            wx.cloud.uploadFile({
+                cloudPath,
+                filePath
               })
-            })
+              .then(res => {
+                const src = res.fileID
+                list.push(src)
+                // 如果还有照片，继续上传
+                i++
+                if (i < l) {
+                  clearTimeout(timer)
+                  timer = setTimeout(() => {
+                    upload(l)
+                  }, 50)
+                } else {
+                  timer = null
+                  wx.hideLoading()
+                  callback(list)
+                }
+              })
+              .catch(error => {
+                wx.hideLoading()
+                wx.showToast({
+                  icon: 'none',
+                  title: '上传失败',
+                })
+              })
+          }
+          upload(len)
         },
         fail: e => {
           console.error(e)
@@ -241,20 +282,24 @@ export default {
     width: 23.5%;
     height: 68px;
     background-color: #F8F8F8;
+
     .img {
       display: block;
       width: 100%;
       height: 68px;
       vertical-align: middle;
     }
-    .my_cancel{
+
+    .my_cancel {
       position: absolute;
       top: -10px;
       right: 0;
     }
-    .img_title{
-      text-align: center; 
+
+    .img_title {
+      text-align: center;
     }
+
     &:nth-child(4n+4) {
       margin-right: 0;
     }
@@ -264,28 +309,32 @@ export default {
 .add_item_info {
   padding: 14px;
   text-align: center;
+
   .img {
-      display: block;
-      margin: 0 auto 20px;
-      width: 100px;
-      height: 100px;
-      vertical-align: middle;
-      background-color: #F8F8F8;
-    }
+    display: block;
+    margin: 0 auto 20px;
+    width: 100px;
+    height: 100px;
+    vertical-align: middle;
+    background-color: #F8F8F8;
+  }
 }
-.my_popup{
+
+.my_popup {
   overflow: hidden;
   padding: 6px 14px;
-  border-top: 1rpx solid #F8F8F8; 
-  border-bottom: 1rpx solid #F8F8F8; 
+  border-top: 1rpx solid #F8F8F8;
+  border-bottom: 1rpx solid #F8F8F8;
   text-align: left;
-  .my_popup_title{
+
+  .my_popup_title {
     color: #000;
     float: left;
     height: 26px;
     line-height: 26px;
   }
 }
+
 .fixed_btn {
   position: fixed;
   bottom: 0;
